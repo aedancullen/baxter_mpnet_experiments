abe506d2326a55e45982c7befa5ef9484bff9d4b
Aedan Cullen <aedan@pig.vision>
2021-02-23 12:51:55 -0600

    Set dropout=0.001

--- architectures.py
+++ architectures.py
@@ -10,13 +10,13 @@ class MLP(nn.Module):
 	def __init__(self, input_size, output_size):
 		super(MLP, self).__init__()
 		self.fc = nn.Sequential(
-                    nn.Linear(input_size, 1280), nn.PReLU(), #nn.Dropout(),
-                    nn.Linear(1280, 896), nn.PReLU(), #nn.Dropout(),
-                    nn.Linear(896, 512), nn.PReLU(), #nn.Dropout(),
-                    nn.Linear(512, 384), nn.PReLU(), #nn.Dropout(),
-                    nn.Linear(384, 256), nn.PReLU(), #nn.Dropout(),
-                    nn.Linear(256, 128), nn.PReLU(), #nn.Dropout(),
-                    nn.Linear(128, 64), nn.PReLU(), #nn.Dropout(),
+                    nn.Linear(input_size, 1280), nn.PReLU(), nn.Dropout(p=0.001),
+                    nn.Linear(1280, 896), nn.PReLU(), nn.Dropout(p=0.001),
+                    nn.Linear(896, 512), nn.PReLU(), nn.Dropout(p=0.001),
+                    nn.Linear(512, 384), nn.PReLU(), nn.Dropout(p=0.001),
+                    nn.Linear(384, 256), nn.PReLU(), nn.Dropout(p=0.001),
+                    nn.Linear(256, 128), nn.PReLU(), nn.Dropout(p=0.001),
+                    nn.Linear(128, 64), nn.PReLU(), nn.Dropout(p=0.001),
                     nn.Linear(64, 32), nn.PReLU(),
                     nn.Linear(32, output_size))
 
